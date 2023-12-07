778c5605cb194a99cfb2f6d355a0c2fa0053fa90
Andreas Thoren <andreasthoren79@gmail.com>
2023-12-07 20:35:37 +0100

    More work on documentation webpage. I'm now using showdown to parse markdown client side

---
+++ docs/scripts/script.js
@@ -0,0 +1,24 @@
+const urlReadme = "./docs/README.md"
+const urlUserInfo = "./docs/user_info_swedish.md"
+const aReadme = document.getElementById("a-readme");
+const aUserInfo = document.getElementById("a-user-info");
+const main = document.getElementById("main");
+
+let converter = new showdown.Converter();
+converter.setFlavor('github');
+
+async function fetchConvertMarkdown(url) {
+  const x = await fetch(url);
+  const y = await x.text();
+  return converter.makeHtml(y);
+}
+
+aReadme.addEventListener("click", (event) => {
+  event.preventDefault();
+  fetchConvertMarkdown(urlReadme).then(parsedHtml => main.innerHTML = parsedHtml);
+});
+
+aUserInfo.addEventListener("click", (event) => {
+  event.preventDefault();
+  fetchConvertMarkdown(urlUserInfo).then(parsedHtml => main.innerHTML = parsedHtml)
+});
